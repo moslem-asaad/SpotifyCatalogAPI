3e979ddadff8240ef15df3643f28aec41a708810
moslem-asaad <moslem@post.bgu.ac.il>
2024-12-10 13:24:58 +0200

    first step

---
+++ src/main/java/com/example/catalog/controller/PracticeController.java
@@ -0,0 +1,54 @@
+package com.example.catalog.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class PracticeController {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @GetMapping("/capitalize")
+    public String capitalizeName(String name) {
+        return StringUtils.capitalize(name);
+    }
+
+
+    /**
+     * @return sample sorted songs list
+     */
+    @GetMapping("/sampleSongsNames")
+    public List<String> getSampleSongs() {
+        List<String> songNames = Arrays.asList(
+                "Kill Bill", "Daydreaming", "Havana (feat. Young Thug)"
+        );
+
+        // TODO sort songs by names
+
+        return songNames;
+    }
+
+
+    /**
+     * @return the song object with the highest popularity
+     * @throws IOException
+     */
+    @GetMapping("/mostPopularSongs")
+    public Map<String, Object> getMostPopularSongs() throws IOException {
+        ClassPathResource resource = new ClassPathResource("data/popular_songs.json");
+        JsonNode songsNode = objectMapper.readTree(resource.getFile());
+        List<Map<String, Object>> songsList = objectMapper.convertValue(songsNode, List.class);
+
+        return songsList.get(0);  // TODO return the song with the highest popularity
+    }
+
+}
