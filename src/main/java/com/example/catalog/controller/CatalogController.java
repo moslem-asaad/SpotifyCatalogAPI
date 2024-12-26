b5941079013a4a0799ccba1d6cc246b668df89ce
moslem-asaad <moslem@post.bgu.ac.il>
2024-12-26 09:38:53 +0200

    working on catalog controller

--- src/main/java/com/example/catalog/controller/CatalogController.java
+++ src/main/java/com/example/catalog/controller/CatalogController.java
@@ -1,5 +1,6 @@
 package com.example.catalog.controller;
 
+import com.example.catalog.utils.CatalogUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -8,9 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 import com.example.catalog.utils.SpotifyUtils;
 
@@ -19,7 +23,7 @@ public class CatalogController {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @GetMapping("/popularSongs")
+   /* @GetMapping("/popularSongs")
     public ResponseEntity<JsonNode> getPopularSongs() throws IOException {
         ObjectNode errorResponse = objectMapper.createObjectNode();
         try {
@@ -36,7 +40,7 @@ public class CatalogController {
             errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
             return new ResponseEntity<> (errorResponse,HttpStatus.INTERNAL_SERVER_ERROR );
         }
-    }
+    }*/
 
     @GetMapping("/popularArtists")
     public ResponseEntity<JsonNode> getPopularArtists() throws IOException {
@@ -104,4 +108,109 @@ public class CatalogController {
         return popularSongs.isEmpty();
     }
 
+    @GetMapping("/popularSongs/filter")
+    public ResponseEntity<?> filterSongsByNameOrPopularity(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false, defaultValue = "0") int minPopularity) throws IOException {
+        ObjectNode errorResponse = objectMapper.createObjectNode();
+        try {
+            if (emptyPopularSongsHandler()) {
+                errorResponse.put("message", "Service temporarily unavailable");
+                errorResponse.put("status", HttpStatus.SERVICE_UNAVAILABLE.value());
+                return new ResponseEntity<>(errorResponse, HttpStatus.SERVICE_UNAVAILABLE);
+            }
+            ClassPathResource resource = new ClassPathResource("data/popular_songs.json");
+            JsonNode allSongs = objectMapper.readTree(resource.getFile());
+            List<JsonNode> songs = StreamSupport.stream(allSongs.spliterator(), false).toList();
+            CatalogUtils catalogUtils = new CatalogUtils();
+            if (name != null && !name.isEmpty()) {
+                songs = songs.stream()
+                        .filter(song -> song.get("name").asText().equalsIgnoreCase(name))
+                        .toList();
+            }
+            songs = catalogUtils.filterSongsByPopularity(songs, minPopularity);
+            return new ResponseEntity<>(songs, HttpStatus.OK);
+        } catch (Exception e) {
+            errorResponse.put("message", "Internal server error occurred");
+            errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/songs/mostRecent")
+    public  ResponseEntity<JsonNode> getMostRecentSong() throws IOException{
+        ObjectNode errorResponse = objectMapper.createObjectNode();
+        try {
+            if (emptyPopularSongsHandler()) {
+                errorResponse.put("message", "Service temporarily unavailable");
+                errorResponse.put("status", HttpStatus.SERVICE_UNAVAILABLE.value());
+                return new ResponseEntity<>(errorResponse, HttpStatus.SERVICE_UNAVAILABLE);
+            }
+            ClassPathResource resource = new ClassPathResource("data/popular_songs.json");
+            JsonNode allSongs = objectMapper.readTree(resource.getFile());
+            List<JsonNode> songs = StreamSupport.stream(allSongs.spliterator(), false).toList();
+            CatalogUtils catalogUtils = new CatalogUtils();
+            JsonNode mostRecentSong = catalogUtils.getMostRecentSong(songs);
+            return new ResponseEntity<>(mostRecentSong, HttpStatus.OK);
+        }catch (Exception e) {
+            errorResponse.put("message", "Internal server error occurred");
+            errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/songs/longest")
+    public  ResponseEntity<JsonNode> getLongestSong() throws IOException{
+        ObjectNode errorResponse = objectMapper.createObjectNode();
+        try {
+            if (emptyPopularSongsHandler()) {
+                errorResponse.put("message", "Service temporarily unavailable");
+                errorResponse.put("status", HttpStatus.SERVICE_UNAVAILABLE.value());
+                return new ResponseEntity<>(errorResponse, HttpStatus.SERVICE_UNAVAILABLE);
+            }
+            ClassPathResource resource = new ClassPathResource("data/popular_songs.json");
+            JsonNode allSongs = objectMapper.readTree(resource.getFile());
+            List<JsonNode> songs = StreamSupport.stream(allSongs.spliterator(), false).toList();
+            CatalogUtils catalogUtils = new CatalogUtils();
+            JsonNode mostRecentSong = catalogUtils.getLongestSong(songs);
+            return new ResponseEntity<>(mostRecentSong, HttpStatus.OK);
+        }catch (Exception e) {
+            errorResponse.put("message", "Internal server error occurred");
+            errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/popularSongs")
+    public ResponseEntity<?> getPopularSongs(@RequestParam(defaultValue = "0") int offset,
+                                    @RequestParam(defaultValue = "-1") int limit) throws IOException {
+
+        ObjectNode errorResponse = objectMapper.createObjectNode();
+        try {
+            if(emptyPopularSongsHandler()) {
+                errorResponse.put("message", "Service temporarily unavailable");
+                errorResponse.put("status", HttpStatus.SERVICE_UNAVAILABLE.value());
+                return new ResponseEntity<>(errorResponse, HttpStatus.SERVICE_UNAVAILABLE);
+            }
+
+            ClassPathResource resource = new ClassPathResource("data/popular_songs.json");
+            JsonNode allSongs = objectMapper.readTree(resource.getFile());
+            List<JsonNode> songs = StreamSupport.stream(allSongs.spliterator(), false).toList();
+            int fromIndex = Math.min(offset, songs.size());
+            int toIndex = (limit == -1) ? songs.size() : Math.min(offset + limit, songs.size());
+            if (fromIndex > toIndex) {
+                errorResponse.put("message", "Invalid offset or limit values");
+                errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+                return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+            }
+            List<JsonNode> paginatedSongs = songs.subList(fromIndex,toIndex);
+
+            return new ResponseEntity<>(paginatedSongs, HttpStatus.OK);
+        }catch (Exception e){
+            errorResponse.put("message", "Internal server error occurred");
+            errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return new ResponseEntity<> (errorResponse,HttpStatus.INTERNAL_SERVER_ERROR );
+        }
+    }
+
 }
+
